c0c2dd991125c0b06aa7fae923873e2b6eb72df2
Tsvetkoffff <tsvetkoffff@gmail.com>
2021-09-10 23:45:27 +0300

    Fetch data and setState logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React, { useEffect, useState } from 'react';
-import { getStoresIds } from './services/services';
-import { Story } from './components/Story';
+import { getStoresIds, getStory } from './services/services';
+import Story from './components/Story';
 
 function App() {
-  const [storyIds, setStoryIds] = useState([]);
+  const [storeys, setStoreys] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    getStoresIds().then((data) => setStoryIds(data));
+    async function fetchData() {
+      const ids = await getStoresIds();
+      const cutIds = ids.slice(0, 50);
+      console.log(cutIds);
+      const storeysArr = await Promise.all(
+        cutIds.map((id) => {
+          return getStory(id).then((story) => story);
+        })
+      );
+      setStoreys(storeysArr);
+      setIsLoading(!isLoading)
+    }
+    fetchData();
   }, []);
 
+  if(!isLoading){
   return (
     <div className='container mt-3'>
-      <ol>
-        {storyIds.map((id, i) => {
-          return i < 20 ? <Story key={id} storyId={id} /> : null;
-        })}
-      </ol>
+      <div className='list-group'>
+        {storeys.map((s) => (
+          <Story
+            key={s.id}
+            title={s.title}
+            time={s.time}
+            by={s.by}
+            score={s.score}
+          />
+        ))}
+      </div>
     </div>
   );
+        } else {
+          return <p>Loading</p>
+        }
 }
 
 export default App;
