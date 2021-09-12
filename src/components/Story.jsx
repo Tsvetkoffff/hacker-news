ac5e094a02152d8bde4fdef53c1412774a9fddfa
Tsvetkoffff <tsvetkoffff@gmail.com>
2021-09-12 16:02:58 +0300

    Refactoring and add use TimeStamp formatter

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import {mapDateFromStamp} from '../mappers/mapDateFromStamp'
 
-export default ({ by, time, score, title, url }) => (
+export const Story = ({ by, score, title, url, time}) => (
   <a
     href={url}
     className='list-group-item list-group-item-action'
     aria-current='true'
   >
     <div className='d-flex w-100 justify-content-between'>
-      <h5 className='mb-1'> { title } </h5>
-      <small>{time}</small>
+      <h5 className='mb-1'> {title} </h5>
+      <small>{mapDateFromStamp(time)}</small>
     </div>
     <p className='mb-1'>{by}</p>
     <small>{score}</small>
   </a>
 );
+
+export default Story;
