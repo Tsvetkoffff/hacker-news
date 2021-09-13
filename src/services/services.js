ce553c69a6659dd063e0bf400eed6113cb2ef6cf
Tsvetkoffff <tsvetkoffff@gmail.com>
2021-09-13 16:34:55 +0300

    Completely rewrote the API request service

--- src/services/services.js
+++ src/services/services.js
@@ -4,12 +4,18 @@ const BASE_URL = 'https://hacker-news.firebaseio.com/v0';
 const STORIES_IDS_URL = `${BASE_URL}/topstories.json`;
 const STORY_URL = `${BASE_URL}/item/`;
 
-export const getStoresIds = async () => {
-  const result = await axios.get(STORIES_IDS_URL);
-  return result.data;
-};
-
-export const getStory = async (storyId) => {
-  const result = await axios.get(`${STORY_URL}${storyId}.json`);
-  return result.data;
+export const getStoreys = async () => {
+  try {
+    const ids = await axios.get(STORIES_IDS_URL);
+    const storeys = await Promise.all(
+      ids.data
+        .slice(0, 100)
+        .map((id) =>
+          axios.get(`${STORY_URL}${id}.json`).then((res) => res.data)
+        )
+    );
+    return storeys.sort((a, b) => a.time - b.time).reverse();
+  } catch (error) {
+    console.error(error);
+  }
 };
