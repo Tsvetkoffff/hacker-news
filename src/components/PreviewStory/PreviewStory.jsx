5c1aaa278581c3c0f32c05e78b71c987b86da2f3
Slava Tsvetkov <tsvetkoffff@gmail.com>
2021-10-13 23:11:44 +0300

    Remove unused imports

--- src/components/PreviewStory/PreviewStory.jsx
+++ src/components/PreviewStory/PreviewStory.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styles from './PreviewStory.module.css';
 import { Card } from 'antd';
 import { mapDateFromStamp } from '../../utils/mapDateFromStamp';
